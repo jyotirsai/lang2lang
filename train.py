df0181eb98e962e75a0f7da3a894cd3ff9a945e5
jyotirsai <54253219+jyotirsai@users.noreply.github.com>
2023-11-22 13:00:45 -0400

    Updates to train

--- train.py
+++ train.py
@@ -1,94 +1,140 @@
 import torch
 import torch.nn as nn
+from model import Transformer
 from pathlib import Path
 from dataset import build_dataloader_and_tokenizers
-from model import build_transformer
-from utils import load_config, weights_file_path
-
-from torch.utils.tensorboard import SummaryWriter
+from utils import get_weights_file_path, latest_weights_file_path
+from tqdm import tqdm
+import mlflow
+import mlflow.pytorch
 from tqdm import tqdm
 
 
-def train_model(config):
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    print(f"Using device {device}")
+def train(config):
+    device = "cuda" if torch.cuda.is_available() else "mps" if torch.has_mps or torch.backends.mps.is_available() else "cpu"
+    print("Using device: ", device)
 
-    Path(config["model_folder"]).mkdir(parents=True, exist_ok=True)
+    mlflow.start_run()
 
-    (
-        train_dataloader,
-        val_dataloader,
-        src_tokenizer,
-        tgt_tokenizer,
-    ) = build_dataloader_and_tokenizers(config)
-    model = build_transformer(
-        src_tokenizer.get_vocab_size(),
-        tgt_tokenizer.get_vocab_size(),
-        config["seq_len"],
-        config["seq_len"],
-        config["d_model"],
-        config["n_times"],
-        config["h"],
-        config["dropout"],
-        config["d_ff"],
-    ).to(device)
+    train_dataloader, val_dataloader, input_tokenizer, output_tokenizer = build_dataloader_and_tokenizers(config)
 
-    writer = SummaryWriter(config["experiment_name"])
-    optimizer = torch.optim.Adam(model.parameters(), lr=config["lr"], eps=1e-9)
+    model = Transformer(input_tokenizer.get_vocab_size(), output_tokenizer.get_vocab_size(), config['embed_size'], config['seq_len'], config['num_heads'], config['hidden_size'], config['dropout'], config['num_layers'])
+    model.to(device)
+
+    # apply xavier initialization
+    for param in model.parameters():
+        if param.dim() > 1:
+            nn.init.xavier_uniform_(param)
+    
+    Path(f"{config['datasource']}_{config['model_folder']}").mkdir(parents=True, exist_ok=True)
+
+    optimizer = torch.optim.AdamW(model.parameters(), lr=config['learning_rate'])
+    loss_fn = nn.CrossEntropyLoss(ignore_index=input_tokenizer.token_to_id('[PAD]')).to(device)
 
     initial_epoch = 0
     global_step = 0
-    if config["preload"]:
-        model_filename = weights_file_path(config, config["preload"])
-        print(f"Preloading model: {model_filename}")
+    best_epoch = 0
+    prev_val_loss = float('inf')
+    preload = config['preload']
+    model_filename = latest_weights_file_path(config) if preload == 'latest' else get_weights_file_path(config, preload) if preload else None
+    print(model_filename)
+    if model_filename:
+        print(f'Preloading model {model_filename}')
         state = torch.load(model_filename)
-        initial_epoch = state["epoch"] + 1
-        optimizer.load_state_dict(state["optimizer_state_dict"])
-        global_step = state["global_step"]
-
-    loss_fn = nn.CrossEntropyLoss(
-        ignore_index=src_tokenizer.token_to_id("<PAD>"), label_smoothing=0.1
-    ).to(device)
-
-    for epoch in range(initial_epoch, config["num_epochs"]):
+        model.load_state_dict(state['model_state_dict'])
+        initial_epoch = state['epoch'] + 1
+        optimizer.load_state_dict(state['optimizer_state_dict'])
+        global_step = state['global_step']
+        best_epoch = state['best_epoch']
+        prev_val_loss = state['prev_val_loss']
+    else:
+        print('No model to preload, starting from scratch')
+    
+    mlflow.log_param("learning_rate", config['learning_rate'])
+    mlflow.log_param("batch_size", config['batch_size'])
+    mlflow.log_param("epochs", config['num_epochs'])
+    mlflow.log_param("embed_size", config['embed_size'])
+    mlflow.log_param("hidden_size", config['hidden_size'])
+    mlflow.log_param("num_heads", config['num_heads'])
+    mlflow.log_param("num_layers", config['num_layers'])
+    mlflow.log_param("dropout", config['dropout'])
+    mlflow.log_param("seq_len", config['seq_len'])
+    mlflow.log_param("datasource", config['datasource'])
+    
+    mlflow.pytorch.log_model(model, "models")
+    
+    for epoch in range(initial_epoch, config['num_epochs']):
+        torch.cuda.empty_cache()
         model.train()
-        for batch in tqdm(train_dataloader, desc=f"Epoch: {epoch}"):
-            enc_input = batch["encoder_input"].to(device)
-            dec_input = batch["decoder_input"].to(device)
-            enc_mask = batch["encoder_mask"].to(device)
-            dec_mask = batch["decoder_mask"].to(device)
+        train_loss = 0
+        idx = 0
+        best_epoch = 0
+        for encoder_input, decoder_input, label, padding_mask, lookahead_mask in tqdm(train_dataloader, desc=f"Processing Epoch {epoch} for training"):
+            encoder_input = encoder_input.to(device) # (batch_size, seq_len)
+            decoder_input = decoder_input.to(device) # (batch_size, seq_len)
+            label = label.to(device) # (batch_size, seq_len)
+            padding_mask = padding_mask.to(device) # (batch_size, 1, 1, seq_len)
+            lookahead_mask = lookahead_mask.to(device) # (batch_size, 1, seq_len, seq_len)
 
-            output = model.forward(enc_input, enc_mask, dec_input, dec_mask)
+            encoder_output = model.encode(encoder_input, padding_mask)
+            decoder_output = model.decode(decoder_input, encoder_output, padding_mask, lookahead_mask)
+            output = model.predict(decoder_output)
 
-            label = batch["label"].to(device)
+            loss = loss_fn(output.reshape(-1, output.shape[-1]), label.reshape(-1))
+            train_loss += loss.item()
 
-            loss = loss_fn(
-                output.view(-1, tgt_tokenizer.get_vocab_size()), label.view(-1)
-            )
-            print(f"loss: {loss.item()}")
-
-            writer.add_scalar("train loss", loss.item(), global_step)
-            writer.flush()
-
-            loss.backward()
-
-            optimizer.step()
             optimizer.zero_grad()
-
+            loss.backward()
+            optimizer.step()
             global_step += 1
 
-        model_filename = weights_file_path(config, f"{epoch}")
-        torch.save(
-            {
-                "epoch": epoch,
-                "model_state_dict": model.state_dict(),
-                "optimizer_state_dict": optimizer.state_dict(),
-                "global_step": global_step,
-            },
-            model_filename,
-        )
+            idx += 1
+            if idx % 100 == 0:
+              print("Train loss: ", train_loss / idx)
+              mlflow.log_metric("Train loss", train_loss / idx)
+            
+        
+        print(f"Epoch Training Loss { train_loss / idx}")
+        mlflow.log_metric("Epoch Training Loss", train_loss / idx)
 
+        # validation
+        model.eval()
+        with torch.no_grad():
+            val_loss = 0
+            idx = 0
+            for encoder_input, decoder_input, label, padding_mask, lookahead_mask in tqdm(val_dataloader, desc=f"Processing Epoch {epoch} for validation"):
+                encoder_input = encoder_input.to(device) # (batch_size, seq_len)
+                decoder_input = decoder_input.to(device) # (batch_size, seq_len)
+                label = label.to(device) # (batch_size, seq_len)
+                padding_mask = padding_mask.to(device) # (batch_size, 1, 1, seq_len)
+                lookahead_mask = lookahead_mask.to(device) # (batch_size, 1, seq_len, seq_len)
 
-if __name__ == "__main__":
-    config = load_config("./config.yaml")
-    train_model(config)
+                encoder_output = model.encode(encoder_input, padding_mask)
+                decoder_output = model.decode(decoder_input, encoder_output, padding_mask, lookahead_mask)
+                output = model.predict(decoder_output)
+
+                loss = loss_fn(output.reshape(-1, output.shape[-1]), label.reshape(-1))
+                val_loss += loss.item()
+
+                idx += 1
+                if idx % 100 == 0:
+                  print("Validation loss: ", val_loss / idx)
+                  mlflow.log_metric("Validation loss", val_loss / idx)
+
+        print(f"Epoch Validation Loss { val_loss / idx}")
+        mlflow.log_metric("Epoch Validation Loss", val_loss / idx)
+
+        if val_loss / len(val_dataloader) < prev_val_loss:
+            best_epoch = epoch
+            prev_val_loss = val_loss / len(val_dataloader)
+
+        model_filename = get_weights_file_path(config, f"{epoch:02d}")
+        torch.save({
+            'epoch': epoch,
+            'model_state_dict': model.state_dict(),
+            'optimizer_state_dict': optimizer.state_dict(),
+            'global_step': global_step,
+            'best_epoch': best_epoch,
+            'prev_val_loss': prev_val_loss
+        }, model_filename)
+        mlflow.end_run()
--- utils.py
+++ utils.py
@@ -1,15 +1,18 @@
-import yaml
 from pathlib import Path
 
 
-def load_config(config_file):
-    with open(config_file, "r") as stream:
-        config = yaml.safe_load(stream)
-    return config
+# code from https://github.com/hkproj/pytorch-transformer/blob/main/config.py
+def get_weights_file_path(config, epoch: str):
+    model_folder = f"{config['datasource']}_{config['model_folder']}"
+    model_filename = f"{config['model_basename']}{epoch}.pt"
+    return str(Path('.') / model_folder / model_filename)
 
-
-def weights_file_path(config, epochs):
-    model_folder = config["model_folder"]
-    model_basename = "model"
-    model_filename = f"{model_basename}{epochs}.pt"
-    return str(Path(".") / model_folder / model_filename)
+# Find the latest weights file in the weights folder
+def latest_weights_file_path(config):
+    model_folder = f"{config['datasource']}_{config['model_folder']}"
+    model_filename = f"{config['model_basename']}*"
+    weights_files = list(Path(model_folder).glob(model_filename))
+    if len(weights_files) == 0:
+        return None
+    weights_files.sort()
+    return str(weights_files[-1])
